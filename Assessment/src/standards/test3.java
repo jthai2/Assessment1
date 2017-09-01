41245037c62ad09bfd2c9d58bbd95b13e4024725
jthai2 <jimmythai35@gmail.com>
2017-09-01 08:24:23 -0500

    update everything

---
+++ Assessment/src/standards/test3.java
@@ -0,0 +1,13 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "comment one of them out");
+		System.out.println("comment one of them out");
+	}
+
+}
